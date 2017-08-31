32ba0775798f47548267e8cb085ed3adc0fa5850
baahpyrs <rathaursinghpa@gmail.com>
2017-09-01 01:49:49 +0530

    CURRENT: getting data, stored in list[class objs], class interface not done; NEXT: store teamCodes etc, build alert mechnsm

--- footballScores.py
+++ footballScores.py
@@ -8,6 +8,72 @@ api_key = sys.argv[1]
 data_URL = "http://api.football-data.org"
 
 
+#
+# class to store a fixture
+
+teamCodes = {}
+
+def getTeamCodes():
+	return 'ss'
+
+
+
+def getJsonData(URL):
+	page = ur.urlopen(data_URL + URL)
+	data = page.read().decode('utf8')
+	json_data = json.loads(data)
+	return json_data
+
+
+class fixture:
+	
+	def __init__(self):
+		self.date = ''
+		self.awayTeamName = ''
+		self.homeTeamName = ''
+		self.gameStatus = ''
+		self.goalsHomeTeam = 0
+		self.goalsAwayTeam = 0
+		self.goalsAwayTeamHalfTime = 0
+		self.goalsHomeTeamsHalfTime = 0
+		self.homeTeamCode = ''
+		self.awayTeamCode = ''
+		self.minutes = 0
+
+	def __str__(self):
+		txt = self.awayTeamName + ' vs ' + self.homeTeamName
+		return txt
+
+	def getDate(self):
+		return self.date
+
+	def getStatus(self):
+		return self.gameStatus
+
+	def getHomeTeamName(self):
+		return self.homeTeamName
+
+
+	def getAwayTeamName(self):
+		return self.awayTeamName
+
+	def getScoreLine(self):
+		txt = str(self.awayTeamCode) + ' ' + str(self.goalsAwayTeam) + ' - ' + str(self.goalsHomeTeam) + ' ' + (self.homeTeamCode)
+		return txt
+
+	def getResult(self):
+		txt = str(self.minutes) + '\n' + self.getScoreLine()
+		return txt
+
+
+
+
+
+fixtures = []
+
+
+
+
 
 
 
@@ -46,15 +112,26 @@ ur.install_opener(opener)
 # 	txt = fixtures['homeTeamName'] + 'vs' + fixtures['awayTeamName']
 # 	print(txt)
 
-page = ur.urlopen(data_URL + '/v1/competitions/445/fixtures')
-data = page.read().decode('utf8')
-json_data = json.loads(data)
+fixturesBPL_URL = '/v1/competitions/445/fixtures'
 
-fixtures = json_data['fixtures']
+json_data = getJsonData(fixturesBPL_URL)
 
-for obj in fixtures:
-	txt = obj['homeTeamName'] + ' vs ' + obj['awayTeamName']
-	print(txt)
+
+fixtures_data = json_data['fixtures']
+
+for obj in fixtures_data:
+	f_obj = fixture()
+	f_obj.date = obj['date']
+	f_obj.awayTeamName = obj['awayTeamName']
+	f_obj.homeTeamName = obj['homeTeamName']
+	f_obj.gameStatus = obj['status']
+	f_obj.goalsAwayTeam = obj['result']['goalsAwayTeam']
+	f_obj.goalsHomeTeam = obj['result']['goalsHomeTeam']
+
+	print(f_obj)
+	print(f_obj.getResult())
+	print('\n')
+	fixtures.append(f_obj)
 
 
 # soup = BeautifulSoup(page, "lxml")
